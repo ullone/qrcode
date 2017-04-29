86a60ee90a87cb652901221f2e511ecc6200c1a8
newlif <2659952905@qq.com>
2017-04-30 01:17:26 +0800

    fix

---
+++ application/api/controller/UserAuth.php
@@ -0,0 +1,19 @@
+<?
+namespace app\api\controller;
+
+header("Content-Type:text/html; charset=utf-8");
+
+use think\Db;
+use think\Cookie;
+use think\Cache;
+use app\index\controller;
+use app\easywechat\Foundation\Application;
+
+class UserAuth {
+  public function __construct () {
+    $app         = new Application($options);
+    $userService = $app->user;
+    $user        = $userService->get($openId);
+    echo $user->nickname;
+  }
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+
+use app\api\controller\UserAuth;
+use \think\View;
+
+class Index extends UserAuth
+{
+    public function index()
+    {
+      echo 'success';
+    }
+}
