e6d432b92eff6760fd6f393639189641cedd1ef9
newlif <2659952905@qq.com>
2017-04-30 15:13:11 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,10 +4,11 @@ namespace app\index\controller;
 use app\api\controller\UserAuth;
 use \think\View;
 
-class Index extends UserAuth
+class Index
 {
     public function index()
     {
+      $test = new UserAuth();
       echo 'success';
     }
 }
