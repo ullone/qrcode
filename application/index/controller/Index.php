5a0e7caf7db52b84e2707f26c1b751a1b2107eb6
Longyang.chen <273139148@qq.com>
2017-04-30 18:44:46 +0800

    fix3

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,7 +8,27 @@ class Index
 {
     public function index()
     {
-      $test = new UserAuth();
-      echo 'success';
+      // $test = new UserAuth();
+      // echo 'success';
+          $echoStr = $_GET["echostr"];
+          $signature = $_GET["signature"];
+          $timestamp = $_GET["timestamp"];
+          $nonce = $_GET["nonce"];
+          $token = "cjiumeng123";
+          $tmpArr = array($token, $timestamp, $nonce);
+          sort($tmpArr, SORT_STRING);
+          $tmpStr = implode( $tmpArr );
+          $tmpStr = sha1( $tmpStr );
+          if( $tmpStr == $signature )
+          {
+            echo $echoStr;
+            exit;
+          }
+
+          else
+          {
+            echo "validate没有通过";
+          }
+
     }
 }
