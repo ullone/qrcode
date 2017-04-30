749d266ba19b64d289f543c91071de2494ff6d88
newlif <2659952905@qq.com>
2017-04-30 17:55:14 +0800

    fix

--- application/api/controller/UserAuth.php
+++ application/api/controller/UserAuth.php
@@ -7,7 +7,7 @@ use think\Db;
 use think\Cookie;
 use think\Cache;
 use app\index\controller;
-use app\EasyWechat\Foundation\Application;
+use app\easywechat\Foundation\Application;
 
 class UserAuth {
   public function __construct () {
