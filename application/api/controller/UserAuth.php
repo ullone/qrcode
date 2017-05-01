64e65e32522904909947dfbda5751167067bcdfb
newlif <2659952905@qq.com>
2017-05-01 16:04:28 +0800

    fix

--- application/api/controller/UserAuth.php
+++ application/api/controller/UserAuth.php
@@ -13,9 +13,14 @@ use app\easywechat\Foundation\Application;
 
 class UserAuth {
   public function __construct () {
+    $options = [];
     $app         = new Application($options);
-    $userService = $app->user;
-    $user        = $userService->get($openId);
-    echo $user->nickname;
+    // 获取 access token 实例
+    $accessToken = $app->access_token; // EasyWeChat\Core\AccessToken 实例
+    $token = $accessToken->getToken(); // token 字符串
+    $token = $accessToken->getToken(true); // 强制重新从微信服务器获取 token.
+    // $userService = $app->user;
+    // $user        = $userService->get($openId);
+    echo $user->token;
   }
 }
