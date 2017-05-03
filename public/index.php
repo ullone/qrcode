380a813624fc44a3175ce3349a061bd365e1ccfb
newlif <2659952905@qq.com>
2017-05-03 23:14:30 +0800

    fix

--- public/index.php
+++ public/index.php
@@ -16,11 +16,11 @@ define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
 //定义命名空间
-\think\Loader::addNamespace([
-    'EasyWeChat' => '/../application/easywechat/overtrue/wechat/src',
-    'Doctrine'   => '/../application/easywechat/doctrine/cache/lib/Doctrine',
-    'Monolog'    => '/../application/easywechat/monolog/monolog/src/Monolog',
-    'Pimple'     => '/../application/easywechat/pimple/pimple/src/Pimple',
-    'Symfony'    => '/../application/easywechat/symfony/http-foundation'
-]);
-// \think\Loader::addNamespace('EasyWeChat','/../application/easywechat/overtrue/wechat/src');
+// \think\Loader::addNamespace([
+//     'EasyWeChat' => '/../application/easywechat/overtrue/wechat/src',
+//     'Doctrine'   => '/../application/easywechat/doctrine/cache/lib/Doctrine',
+//     'Monolog'    => '/../application/easywechat/monolog/monolog/src/Monolog',
+//     'Pimple'     => '/../application/easywechat/pimple/pimple/src/Pimple',
+//     'Symfony'    => '/../application/easywechat/symfony/http-foundation'
+// ]);
+\think\Loader::addNamespace('abc','../extend/easywechat');
