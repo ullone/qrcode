b0ff6043298b82130b6c7ea24024d4b340b62daa
newlif <2659952905@qq.com>
2017-05-03 23:51:14 +0800

    fix

--- application/api/controller/UserAuth.php
+++ application/api/controller/UserAuth.php
@@ -3,6 +3,7 @@ namespace app\api\controller;
 
 header("Content-Type:text/html; charset=utf-8");
 
+require_once __DIR__ . '/../../../extend/easywechat/autoload.php';
 // include '/webdata/userAPI/vendor/autoload.php';
 
 use think\Db;
@@ -10,20 +11,18 @@ use think\Cookie;
 use think\Cache;
 use app\index\controller;
 // use EasyWeChat\Foundation\Application;
-use abc\Test;
+use EasyWeChat\Foundation as Foundation;
 
 class UserAuth {
   public function __construct () {
-    // $options = [];
-    // $app         = new Application($options);
-    // // 获取 access token 实例
-    // $accessToken = $app->access_token; // easywechat\Core\AccessToken 实例
-    // $token = $accessToken->getToken(); // token 字符串
-    // $token = $accessToken->getToken(true); // 强制重新从微信服务器获取 token.
-    // // $userService = $app->user;
-    // // $user        = $userService->get($openId);
-    // echo $user->token;
-    $tmp = new Test();
-    $tmp->index();
+    $options = [];
+    $app         = new Foundation\Application($options);
+    // 获取 access token 实例
+    $accessToken = $app->access_token; // easywechat\Core\AccessToken 实例
+    $token = $accessToken->getToken(); // token 字符串
+    $token = $accessToken->getToken(true); // 强制重新从微信服务器获取 token.
+    // $userService = $app->user;
+    // $user        = $userService->get($openId);
+    echo $user->token;
   }
 }
--- application/extra/haha/Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace abc;
-
-class Test {
-  public function index () {
-    echo "haha";
-  }
-}
