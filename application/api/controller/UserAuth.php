7e7bb9f0955c902a4e67f9403165fdd8db8cdee2
newlif <2659952905@qq.com>
2017-05-04 20:47:32 +0800

    fix

--- application/api/controller/UserAuth.php
+++ application/api/controller/UserAuth.php
@@ -30,7 +30,7 @@ class UserAuth {
     $app = new Foundation\Application($options);
     // 从项目实例中得到服务端应用实例。
     $server = $app->server;
-    $server->setMessageHandler(function ($message) {
+    $server->setessageHandler(function ($message) {
         // $message->FromUserName // 用户的 openid
         // $message->MsgType // 消息类型：event, text....
         $this->me = "您好！欢迎关注我!";
