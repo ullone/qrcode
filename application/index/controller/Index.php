1537c7e6ae01ea36137cf0975b6249701226fccf
Longyang.chen <273139148@qq.com>
2017-05-07 11:26:02 +0800

    fix3

---
+++ application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+
+use app\api\controller\UserAuth;
+use app\api\controller\CheckToken;
+use \think\View;
+
+class Index
+{
+    public function index() {
+      $test = new UserAuth();
+      // echo 'success';
+      // $check = new CheckToken();
+
+    }
+}
