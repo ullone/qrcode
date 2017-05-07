0ec8a090f9c5480b42baa95a1b071c779f0fbee1
Longyang.chen <273139148@qq.com>
2017-05-07 10:00:21 +0800

    fix3

--- application/api/controller/UserAuth.php
+++ application/api/controller/UserAuth.php
@@ -32,9 +32,13 @@ class UserAuth {
     //
     // echo 'success';
     $app   = new Foundation\Application($options);
-    $response = $app->oauth->scopes(['snsapi_base'])
-                          ->redirect();
-    $response->send();
+    if(empty($_GET['code'])){
+      $response = $app->oauth->scopes(['snsapi_base'])
+      ->redirect();
+      $response->send();
+    }
+    $user = $app->oauth->user();
+    return $user->getId();
     // // 获取 access token 实例
     // $accessToken = $app->access_token; // EasyWeChat\Core\AccessToken 实例
     // // $token = $accessToken->getToken(); // token 字符串
