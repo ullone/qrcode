53991bf6089223bb236a20b3164b3fdf1183bc46
Longyang.chen <273139148@qq.com>
2017-05-14 13:21:44 +0800

    fix3

--- application/api/controller/UserAuth.php
+++ application/api/controller/UserAuth.php
@@ -25,7 +25,8 @@ class UserAuth {
       //应用有app_code
       $this->callBackUserInfo($_GET['user_code'], $this->state);
     }else {
-      echo $this->getOpenId();
+      // echo $this->getOpenId();
+      $this->setMenu();
     }
   }
   private function callBackUserInfo($user_code,$last_url){
@@ -41,10 +42,10 @@ class UserAuth {
     $options = [
       'debug'    => true,
       //测试服务号
-      // 'app_id'   => 'wxfb396a8777e67439',
-      // 'secret'   => '758831403d20fecd8b0ac6334779b3a4',
-      'app_id'   => 'wx4527e7885d91c9e6',
-      'secret'   => '377fcdd3e15e5bc0efc78b6b48004ddc',
+      'app_id'   => 'wxfb396a8777e67439',
+      'secret'   => '758831403d20fecd8b0ac6334779b3a4',
+      // 'app_id'   => 'wx4527e7885d91c9e6',
+      // 'secret'   => '377fcdd3e15e5bc0efc78b6b48004ddc',
       'token'    => 'mateor1newlif2cjiumeng3',
       // 'state'    => 'test',
       'log'      => [
@@ -63,5 +64,54 @@ class UserAuth {
     //执行跳转，重定向操作
     $operate = new Operate($user->getId());
     $operate->index();
+  }
+
+  public function setMenu(){
+    $options = [
+      'debug'    => true,
+      //测试服务号
+      'app_id'   => 'wxfb396a8777e67439',
+      'secret'   => '758831403d20fecd8b0ac6334779b3a4',
+      // 'app_id'   => 'wx4527e7885d91c9e6',
+      // 'secret'   => '377fcdd3e15e5bc0efc78b6b48004ddc',
+      'token'    => 'mateor1newlif2cjiumeng3',
+      // 'state'    => 'test',
+      'log'      => [
+        'level'  => 'debug',
+        'file'   => '/tmp/easywechat.log'
+      ],
+    ];
+    //
+    // echo 'success';
+    $app   = new Foundation\Application($options);
+    if(empty($_GET['code'])){
+      $response = $app->oauth->scopes(['snsapi_base'])->redirect($this->state);
+      $response->send();
+    }
+    $menu = $app->menu;
+    $buttons = [
+    [
+        "type" => "view",
+        "name" => "今日歌曲",
+        "url" => "菜单"
+    ],
+    [
+        "name"       => "菜单",
+        "sub_button" => [
+            [
+                "type" => "view",
+                "name" => "商城",
+                "url"  => "http://dxzshop.cjiumeng.com/"
+            ],
+            [
+                "type" => "view",
+                "name" => "社区",
+                "url"  => "http://dxzchat.cjiumeng.com/"
+            ],
+        ],
+    ],
+  ];
+  $menu->add($buttons);
+
   }
 }
