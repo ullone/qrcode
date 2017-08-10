ea24eee774136af47e567af4c9144f447966d4f6
ullone <ullone@163.com>
2017-08-10 16:07:46 +0800

    fix

--- application/api/controller/UserAuth.php
+++ application/api/controller/UserAuth.php
@@ -104,6 +104,7 @@ class UserAuth {
     $ticket = $result->ticket;// 或者 $result['ticket']
     $expireSeconds = $result->expire_seconds; // 有效秒数
     $url = $result->url; // 二维码图片解析后的地址，开发者可根据该地址自行生成需要的二维码图片
+    var_dump($url);die ;
     echo '<img src="'.$url.'">';
   }
 
